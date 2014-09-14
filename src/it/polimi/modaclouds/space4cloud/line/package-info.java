59071144b19caa2646c5bd8213cc58d633841e10
GiovanniPaoloGibilisco <gibbo87@gmail.com>
2014-09-14 23:38:32 +0200

    ignored line

--- src/it/polimi/modaclouds/space4cloud/line/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.7 
-// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2014.09.14 alle 04:44:01 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.modaclouds.eu/xsd/2013/6/lineResult", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package it.polimi.modaclouds.space4cloud.line;
