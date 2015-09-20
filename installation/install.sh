5b35e59f1c604dfaed35d6ff9ff008c17aab5309
Riccardo B. Desantis <effetti@gmail.com>
2015-09-20 17:52:48 +0200

    Updated the docker files.

--- installation/install.sh
+++ installation/install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+CORES=2
+MEMORY=2048
+DISK=10000
+
 if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X
     docker-machine kill s4c
     docker-machine rm s4c
-    docker-machine create --driver virtualbox --virtualbox-cpu-count 2 --virtualbox-memory 2048 s4c
+    docker-machine create --driver virtualbox --virtualbox-cpu-count $CORES --virtualbox-memory $MEMORY --virtualbox-disk-size $DISK s4c
     eval "$(docker-machine env s4c)"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Windows
