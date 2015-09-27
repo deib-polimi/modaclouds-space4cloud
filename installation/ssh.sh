463a26160b0c6678e1fbcf5000944723bd9c7b67
Riccardo B. Desantis <effetti@gmail.com>
2015-09-27 13:23:35 +0200

    Updated the docker files

--- installation/ssh.sh
+++ installation/ssh.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-PORT="$1"
+IMAGE="$1"
+VERSION="$2"
+
+IP="localhost"
 
 if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X
+    eval "$(docker-machine env s4c)"
     IP=`docker-machine ip s4c`
-    ssh root@$IP -o StrictHostKeyChecking=no -p $PORT -i id_rsa
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Linux
-    ssh root@localhost -o StrictHostKeyChecking=no -p $PORT -i id_rsa
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Windows
     echo "Windows not supported yet."
     exit -1
 fi
+
+PORT=`docker port $IMAGE$VERSION 22 | awk '{ split($1, s, ":"); print s[2]; }'`
+
+ssh -x root@$IP -o StrictHostKeyChecking=no -p $PORT -i $IMAGE/id_rsa
