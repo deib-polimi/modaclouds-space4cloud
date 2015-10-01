5f2a2cca6f85a0d50bdaa112716628b7252ce35a
Riccardo B. Desantis <effetti@gmail.com>
2015-10-01 17:55:01 +0200

    Updated the binaries

--- installation/install-docker.sh
+++ installation/install-docker.sh
@@ -2,14 +2,26 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X
-    if [ "$(which brew)" == "brew not found" ]; then
+    BREW_EXISTS=`which brew | grep "/"`
+    if [ -z "$BREW_EXISTS" ]; then
         echo "You need to do it by yourself, considering that homebrew isn't installed."
         exit -1
     else
+        DOCKER_EXISTS=`which docker | grep "/"`
+        DOCKER_MACHINE_EXISTS=`which docker-machine | grep "/"`
+        VIRTUALBOX_EXISTS=`which virtualbox | grep "/"`
         brew update
-        brew install docker
-        brew install docker-machine
-        brew cask install virtualbox
+        brew upgrade
+        if [ -z "$DOCKER_EXISTS" ]; then
+            brew install docker
+        fi
+        if [ -z "$DOCKER_MACHINE_EXISTS" ]; then
+            brew install docker-machine
+        fi
+        if [ -z "$VIRTUALBOX_EXISTS" ]; then
+            brew tap Caskroom/cask
+            brew cask install virtualbox
+        fi
     fi
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Windows
@@ -21,5 +33,5 @@ elif [ "$(uname)" == "Linux" && "$(which docker)" == "docker not found" ]; then
     sudo apt-get install -y curl
     curl -sSL https://get.docker.com/ | sh
     sudo usermod -aG docker `whoami`
-    newgrp docker
+    echo "Please logout and login again to use docker without sudo."
 fi
